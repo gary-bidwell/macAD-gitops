0fee426bf47e5c8d66ad7de50f3a15ffea56adb0
gary-bidwell <137780938+gary-bidwell@users.noreply.github.com>
2026-04-20 16:21:14 +0100

    lesson 10

---
+++ platforms/windows/scripts/warp-config.ps1
@@ -0,0 +1,11 @@
+Write-Host "Configuring WARP! Here's a dino
+               __
+              / _)
+     _.----._/ /
+    /         /
+ __/ (  | (  |
+/__.-'|_|--|_|
+"
+url: https://downloads.cloudflareclient.com/v1/download/windows/version/2025.2.664.0
+post_install_script:
+   path: ../scripts/warp-config.ps1
